296aca0d234718c7cf60e661fea8c01448c8d608
Gustavo Passinato <gustavopassinato@ufg.br>
2022-06-22 15:36:26 -0300

    Set up configurations

--- src/main/java/br/com/estoqueBr/EstoqueBrApplication.java
+++ src/main/java/br/com/estoqueBr/EstoqueBrApplication.java
@@ -2,12 +2,19 @@ package br.com.estoqueBr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EstoqueBrApplication {
+public class EstoqueBrApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstoqueBrApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EstoqueBrApplication.class);
 	}
 
 }
