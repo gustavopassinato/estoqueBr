50c3e1d071dead00b5601d08417ee7a5d0888f9f
Gustavo Passinato Neves <passinato11@gmail.com>
2022-06-21 21:17:22 -0300

    Primeiro commit

---
+++ src/main/java/br/com/estoqueBr/EstoqueBrApplication.java
@@ -0,0 +1,13 @@
+package br.com.estoqueBr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueBrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueBrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estoqueBr/EstoqueBrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estoqueBr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueBrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
