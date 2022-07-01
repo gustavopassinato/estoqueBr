bc32d238182d740dea12e761d0731cf42a004179
Gustavo Passinato <gustavopassinato@ufg.br>
2022-07-01 18:20:50 -0300

    Arquivo restante

--- src/main/java/br/com/estoqueBr/controler/cadastro/NotaFiscalController.java
+++ src/main/java/br/com/estoqueBr/controler/cadastro/NotaFiscalController.java
@@ -2,15 +2,21 @@ package br.com.estoqueBr.controler.cadastro;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.estoqueBr.model.dto.NotaFiscalDto;
 
 @Controller
 public class NotaFiscalController {
 
-	@GetMapping("/cadastro/nota_fiscal")
+	@GetMapping("/cadastro/entrada/nota_fiscal")
+	public String preparaNotaFiscal(NotaFiscalDto notaFiscalDto) {
+		
+		return "cadastro/entrada/nota_fiscal";
+	}
+	
+	@PostMapping("/cadastro/entrada/nota_fiscal")
 	public String criaNotaFiscal(NotaFiscalDto notaFiscalDto) {
 		
-		return "cadastro/nota_fiscal";
 	}
 }
